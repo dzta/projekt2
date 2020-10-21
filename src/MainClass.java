c15d925c41f1ed0bd9e4cfaa5a97d15e745c80d5
AdrianCo97 <adrian.corazza@outlook.com>
2020-10-21 16:27:48 +0200

    Testuppdatering

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,8 +2,7 @@
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Projektuppgift 2
+		System.out.println("Hello World, testing testing");
 	}
 
 }
