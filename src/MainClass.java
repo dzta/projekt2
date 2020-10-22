7c6684ffd35d8b5fb2c3e4d87fc5054a9c9dccd3
AdrianCo97 <adrian.corazza@outlook.com>
2020-10-22 12:33:27 +0200

    La till en commandlist och en Parsearguments metod.

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,50 @@
 
 public class MainClass {
+	
+	enum Command{
+		LIST,
+		CHECKHOUT,
+		CHECKIN,
+		REGISTER,
+		DEREGISTER,
+		INFO,
+		QUIT,
+		UNKNOWN
+	}
+	
+	public static Command parseCommand(String userInput) {
+		String commandString = userInput.split(" ")[0];
+		switch(commandString) {
+		case "list":
+			return Command.LIST;
+		case "checkout":
+			return Command.CHECKOUT;
+		case "checkin":
+			return Command.CHECKIN;
+		case "register":
+			return Command.REGISTER;
+		case "deregister":
+			return Command.DEREGISTER;
+		case "info":
+			return Command.INFO;
+		case "quit":
+			return Command.QUIT;
+		default:
+			return Command.UNKNOWN;
+		}
+	}
+	
+	public static String[] parseArguments(String userInput) {
+		String[] commandAndArguments = userInput.split(" ");
+		String[] arguments = new String[commandAndArguments.length - 1];
+		for(int i=1; i < commandAndArguments.length; i++) {
+			arguments[i-1] = commandAndArguments[i];
+		}
+		return arguments;
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		
 	}
 
 }
